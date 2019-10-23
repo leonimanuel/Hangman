90cf9a007f1f4a073de6d100e690fca137bda200
LIManuel <leonmalisov@gmail.com>
2019-10-23 14:57:35 -0400

    added event listener so you can press enter to enter guess

--- index.js
+++ index.js
@@ -12,6 +12,20 @@
 // let random = randomizedPhrase
 // console.log(random[Math.floor(Math.random()*random.length)])
 
+let guessInput = document.getElementById("GuessInput");
+
+// Execute a function when the user releases a key on the keyboard
+guessInput.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("SubmitButton").click();
+  }
+});
+
+
 const topicArray = Object.keys(allTopics)
 // console.log(topicArray)
 
